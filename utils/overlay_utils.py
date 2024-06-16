cd16694996f1c56757eb0efba3e91a460be985e2
Y4rd13 <benjaserrau@gmail.com>
2024-06-16 07:23:04 -0400

    fix: resize icons from overlay_utils

--- utils/overlay_utils.py
+++ utils/overlay_utils.py
@@ -1,14 +1,14 @@
 import tkinter as tk
 import customtkinter as ctk
 import os
-from PIL import Image
+from PIL import Image, ImageTk
 
 def create_overlay(self):
     self.overlay = tk.Toplevel(self.root)
     self.overlay.geometry("100x100")
     self.overlay.overrideredirect(True)  # Remove window decorations
     self.overlay.attributes("-topmost", True)
-    self.overlay.attributes('-alpha', 0.9)  # 3% transparent
+    self.overlay.attributes('-alpha', 0.9)  # 10% transparent
     self.overlay.config(bg="black")
 
     # Set shape to be round
@@ -16,13 +16,24 @@ def create_overlay(self):
     self.overlay_canvas.pack(fill="both", expand=True)
     self.overlay_canvas.create_oval(0, 0, 100, 100, fill="black")
 
-    # Load images for the recording button in the overlay
-    self.overlay_image_record = ctk.CTkImage(light_image=Image.open(os.path.join(os.getcwd(), "assets", "img", "pause-play-00.png")))
-    self.overlay_image_stop = ctk.CTkImage(light_image=Image.open(os.path.join(os.getcwd(), "assets", "img", "pause-play-01.png")))
+    # Create a frame inside the canvas to hold the button
+    self.overlay_frame = ctk.CTkFrame(self.overlay, width=100, height=100, fg_color="black", corner_radius=50)
+    self.overlay_frame.place(relx=0.5, rely=0.5, anchor=tk.CENTER)
+
+    # Load and resize images for the recording button in the overlay
+    icon_path = os.path.join(os.getcwd(), "assets", "img", "icon.png")
+    image_record = Image.open(icon_path)
+    image_record_resized = image_record.resize((50, 50), Image.LANCZOS)
+    self.overlay_image_record = ImageTk.PhotoImage(image_record_resized)
+    
+    icon_stop_path = os.path.join(os.getcwd(), "assets", "img", "pause-play-01.png")
+    image_stop = Image.open(icon_stop_path)
+    image_stop_resized = image_stop.resize((50, 50), Image.LANCZOS)
+    self.overlay_image_stop = ImageTk.PhotoImage(image_stop_resized)
 
     # Central button to record audio in the overlay
-    self.overlay_record_button = ctk.CTkButton(self.overlay_canvas, text="", command=self.toggle_recording, width=50, height=50, image=self.overlay_image_record, fg_color="transparent")
-    self.overlay_record_button.place(relx=0.5, rely=0.5, anchor=tk.CENTER)
+    self.overlay_record_button = ctk.CTkButton(self.overlay_frame, text="", command=self.toggle_recording, width=50, height=50, image=self.overlay_image_record, fg_color="transparent")
+    self.overlay_record_button.pack(expand=True)
 
     self.overlay.withdraw()  # Hide the overlay initially
 
